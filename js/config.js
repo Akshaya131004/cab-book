eb8a2facee884e6b02a2d31cc260d5662061de15
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-10 20:03:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_icDTTjJBR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '546jnckl9ddt88hc9def5d5tlv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        clientSecret: 'v0ma5jaovrchilf9dgqq4eibkbu0dgk9om7tjt8ebf52vhvfsqc', // Add your actual client secret here
+        userPoolId: 'ap-south-1_6Je3cPWJB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '12a9n9n15hv0b6mu8pv7mbkjo6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
