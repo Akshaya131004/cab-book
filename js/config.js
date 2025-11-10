3a6ad9ab5e69f74c210a5234fdad5addc7bf23c8
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-10 19:30:16 +0530

    Update Cognito configuration in config.js
    Updated Cognito user pool configuration and added client secret.

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_20IngKwIw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4q22tg3l26qaahrk0a4tco5h52', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_icDTTjJBR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '546jnckl9ddt88hc9def5d5tlv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: 'v0ma5jaovrchilf9dgqq4eibkbu0dgk9om7tjt8ebf52vhvfsqc', // Add your actual client secret here
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
-
