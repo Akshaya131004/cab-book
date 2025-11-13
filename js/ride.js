da27d0e4656b27254c5c018d9e5c394a7456f6b8
Akshaya131004 <akshayad617@gmail.com>
2025-11-13 11:29:37 +0530

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -1,1 +1,111 @@
+/*global WildRydes _config*/
+
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
+
+(function rideScopeWrapper($) {
+    var authToken;
+
+    // Get Auth Token
+    WildRydes.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
+    });
+
+    // Request Unicorn
+    function requestUnicorn(pickupLocation) {
+        if (!authToken) {
+            alert('Authentication token not available. Please sign in again.');
+            return;
+        if (!_config.api.invokeUrl) {
+            alert('API URL is not configured. Please check config.js.');
+            return;
+        }
+
+        console.log('Auth Token:', authToken);
+        console.log('API URL:', _config.api.invokeUrl);
+
+        $.ajax({
+            method: 'POST',
+           Location.latitude,
+                    Longitude: pickupLocation.longitude
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error requesting ride:', textStatus, ', Details:', errorThrown);
+                console.error('Response:', jqXHR.responseText);
+                           }
+        });
+    }
+
+    // Handle API Response
+    function completeRequest(result) {
+        console.log('Response received from API:', result);
+        var unicorn = result.Unicorn;
+        var pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
+        displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.');
+        animateArrival(function animateCallback() {
+            displayUpdate(unicorn.Name + ' has arrived. Giddy up!');
+            WildRydes.map.unsetLocation();
+            $('#request').prop('disabled', 'disabled');
+            $('#request').text('Set Pickup');
+        });
+    }
+
+    // Register click handler for #request button
+    $(function onDocReady() {
+        $('#request').click(handleRequestClick);
+        authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal      $('.authToken').text(token);
+            }
+        });
+
+        if (!_config.api.invokeUrl) {
+            $('#noApiMessage').show();
+        }
+    });
+
+    function handlePickupChanged() {
+        var requestButton = $('#request');
+        requestButton.text('Request Unicorn');
+        requestButton.prop('disabled', false);
+    }
+
+    function handleRequestClick(event) {
+        pickupLocation = WildRydes.map.selectedPoint;
+        requestUnicorn(pickupLocation);
+    }
+
+    function animateArrival(callback) {
+        var dest = WildRydes.map.selectedPoint;
+        var origin = {};
+
+        if (dest.latitude > WildRydes.map.center.latitude) {
+            origin.latitude = WildRydes.map.extent.minLat;
+        } else {
+           .center.longitude) {
+            origin.longitude = WildRydes.map.extent.minLng;
+        } else {
+            origin.longitude = WildRydes.map.extent.maxLng;
+        }
+
+        WildRydes.map.animate(origin, dest, callback);
+    }
+
+    function displayUpdate(text) {
+        $('#updates').append($('<li>' + text + '</li>'));
+   displayUpdate()` 
+
+
+ 
+
+
+        
 
