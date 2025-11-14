56063c2ae80a250644d1a4e1d4ff018a4dd7a72d
Akshaya131004 <akshayad617@gmail.com>
2025-11-14 11:07:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_fjUilMOEm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5770g8o8kgujpbiqvmm3lajabu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_k0qjMUKxu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4feo83fbmuas4evruq80bvo31i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://onrfdqncg4.execute-api.ap-south-1.amazonaws.com/ryder' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
